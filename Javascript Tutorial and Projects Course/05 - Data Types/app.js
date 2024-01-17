bce58a2f8d69684168e5c7b3969fdd4c2cd30cc5
Breno Yuri <byurifb@gmail.com>
2024-01-17 10:46:32 -0300

    Começando com Arrays

---
+++ Javascript Tutorial and Projects Course/05 - Data Types/app.js
@@ -0,0 +1,9 @@
+
+const text = 'Some Text'
+
+const number = 45
+
+console.log(typeof text);
+console.log(typeof 'Another Text');
+
+console.log(number);
---
+++ Javascript Tutorial and Projects Course/06 - Arrays/app.js
@@ -0,0 +1,18 @@
+
+const friend1 = 'Anna'
+const friend2 = 'Anna'
+const friend3 = 'Anna'
+const friend4 = 'Anna'
+
+// const friends = [friend1, friend2, friend3, friend4]
+
+const friends = ['john', 'piter', 'bob', 'suzy', 45, undefined, null]
+
+let bestFriend = friends[2]
+
+console.log(bestFriend);
+friends[4] = 'anna'
+console.log(friends);
+
+console.log(friends[4]);
+
