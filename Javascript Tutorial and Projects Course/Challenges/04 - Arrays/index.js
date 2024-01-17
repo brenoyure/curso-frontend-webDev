da4d3994e02ce6e163578d39b260c3ff2bd29c2e
Breno Yuri <byurifb@gmail.com>
2024-01-17 11:03:45 -0300

    Fazendo o Desafio de Arrays

--- Javascript Tutorial and Projects Course/Challenges/04 - Arrays/index.js
+++ Javascript Tutorial and Projects Course/Challenges/04 - Arrays/index.js
@@ -1,2 +1,11 @@
 
+const fruits = ['Apple', 'Banana', 'Orange', 'Melon']
 
+fruits[3] = 42
+
+const firstFruit = fruits[0]
+
+fruits[3] = 'Water Melon'
+
+console.log(firstFruit);
+console.log(fruits);
